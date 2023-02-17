938b62f66e17ac9a92ea2c0e507c570a696d1aa9
Xr <69309718+FabuKi-Xr@users.noreply.github.com>
2023-02-18 01:50:02 +0700

    some tags come up with others. มีแท็กที่เป็น ref ติมาด้วย

--- scraping/main.py
+++ scraping/main.py
@@ -1,4 +1,5 @@
 from scraper import Scraper
+from utils import CSVWriter
 
 Province_url ={
     "เชียงใหม่" : "https://th.wikipedia.org/wiki/%E0%B8%A3%E0%B8%B2%E0%B8%A2%E0%B8%8A%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B9%83%E0%B8%99%E0%B8%88%E0%B8%B1%E0%B8%87%E0%B8%AB%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B9%80%E0%B8%8A%E0%B8%B5%E0%B8%A2%E0%B8%87%E0%B9%83%E0%B8%AB%E0%B8%A1%E0%B9%88",
@@ -7,9 +8,13 @@ Province_url ={
     "กาญจนบุรี" : "https://th.m.wikipedia.org/wiki/%E0%B8%A3%E0%B8%B2%E0%B8%A2%E0%B8%8A%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B9%83%E0%B8%99%E0%B8%88%E0%B8%B1%E0%B8%87%E0%B8%AB%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B8%81%E0%B8%B2%E0%B8%8D%E0%B8%88%E0%B8%99%E0%B8%9A%E0%B8%B8%E0%B8%A3%E0%B8%B5",
     "อุดรธานี" : "https://th.m.wikipedia.org/wiki/%E0%B8%A3%E0%B8%B2%E0%B8%A2%E0%B8%8A%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B9%83%E0%B8%99%E0%B8%88%E0%B8%B1%E0%B8%87%E0%B8%AB%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B8%AD%E0%B8%B8%E0%B8%94%E0%B8%A3%E0%B8%98%E0%B8%B2%E0%B8%99%E0%B8%B5",
 }
-regex = r'(?<=title=")วัด.*?(?="|\s)'
-
+regex = r'(?<=title=")วัด\w+.*?(?="|\s|\()'
+Chiang_Mai = r'(?<=id=\"content-collapsible-block-1\")w+.*?(?=</section>)'
 if __name__ == "__main__":
-    for province in Province_url:
-        crawl = Scraper(Province_url[province],regex)
-        print(len(crawl.scrape()))
+    # for province in Province_url:
+    #     crawl = Scraper(Province_url[province],regex)
+    #     writer = CSVWriter(f"./data/{province}.csv","ชื่อวัด")
+    #     writer.write(crawl.scrape())
+    #     writer.close()
+    crawl = Scraper(Province_url["เชียงใหม่"],Chiang_Mai)
+    print(crawl.scrape())
