bfab2244191f805232c472119f6abcf45177db3c
Xr <69309718+FabuKi-Xr@users.noreply.github.com>
2023-03-02 19:00:27 +0700

    update data and regex
    to solve problem วัดไทย in more detail and duplicate information in navigation in role tag
    I have edit my regex by using 2 regex.One uses to cut string that contains temple and
    the another uses to find all temples.

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -5,15 +5,17 @@ class Scraper:
     def __init__(self, url,regex):
         self.url = url
         self.regex = regex
+        self.content = None
 
-    def scrape(self):
+    def crawl(self):
         #open the web page, pass url to urlopen():
         webpage = urlopen(self.url)
-        html = self.read_and_decode_with_utf8_webpage(webpage)
-        return self.find_all_string_with_regex_and_extract_data(html)
+        self.html = self.read_and_decode_with_utf8_webpage(webpage)
+        return self
+    
+    def scrape(self,regex,name=None):
+        self.content = re.findall(self.regex,self.html)[0]
+        return re.findall(regex,self.content)
 
     def read_and_decode_with_utf8_webpage(self, webpage):
         return webpage.read().decode("utf-8")
-    
-    def find_all_string_with_regex_and_extract_data(self, html):
-        return re.findall(self.regex, html)
